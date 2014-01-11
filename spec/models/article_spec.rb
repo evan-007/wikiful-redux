4f883409d747786128271364e22d0c6bab65f5c7
Evan Lloyd <eulloyd@gmail.com>
2014-01-11 19:43:04 +0900

    Fixes error in user-art relationship

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -26,21 +26,7 @@ describe Article do
   	article2 = Article.new(title: "Best article", body: "9 Pulitzer prizes")
   	expect(article2).to_not be_valid #or expect(article2).to have(1).errors_on(:title)
   end
-
-  it "belongs to a user" do
-  	# article = Article.create(title: "Best article", body: "9 Pulitzer prizes", user_id: 1)
-  	# user = mock_model("User", :user_id => 1, :name => "Santa")
-  	# expect(article.user.find(params[:user_id]).name).to eq("Santa")
-  end
-
-  it "is not valid without a user" do
-  	# article = Article.new(title: "Best article", body: "9 Pulitzer prizes")
-  	# article.user = nil
-  	# expect(article).to_not be_valid
-  end
   
-  #this needs a double because we have no user model yet
-
   it "has a category"
 
   it "has many categories"
