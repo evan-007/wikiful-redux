7c19ad21c3bf98d3ab1dacbf5fd77eec8a876744
Evan Lloyd <eulloyd@gmail.com>
2014-01-11 14:59:25 +0900

    Generates empty rails appp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WikifulRedux::Application.config.secret_key_base = 'ca89c070f0767880368754d5ab53b3610ede76d635d2449f153136806e521b7982950adbe7601691190bc4dfe043fdce64c476cee469f66b81aee3a7361860fa'
