2cbeea512eeddda8157261fb9dc93126ad6197d4
Evan Lloyd <eulloyd@gmail.com>
2014-01-12 23:18:00 +0900

    cleans up sessions spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
 	def new
 	end
+
+	def create
+		@user == User.find_by_email(params[:email])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = user.user_id
+			redirect_to articles_path, notice: "Logged in"
+		else
+			render "new"
+		end
+	end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,11 +9,20 @@ describe SessionsController do
 	end
 
 	describe "POST #create" do
-		it "does something"
+		context "correct login information" do
+			it "sets the user session"
+			it "redirects to root"
+		end
+		
+		context "incorrect login information" do
+			it "does not set a session"
+			it "renders the new view"
+		end
 	end
 
 	describe "DELETE #destroy" do
-		it "deletes some stuff"
+		it "closes the session"
+		it "renders root"
 	end
 
 end
