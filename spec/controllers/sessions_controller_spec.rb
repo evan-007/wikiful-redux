affb203498e8826696af74e9a514dcfa46f727e5
Evan Lloyd <eulloyd@gmail.com>
2014-01-12 22:18:36 +0900

    Sets up sessions controller and tests

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe SessionsController do
+	describe "GET #new" do
+		it "renders the new view"
+	end
+
+	describe "POST #create" do
+		it "does something"
+	end
+
+	describe "DELETE #destroy" do
+		it "deletes some stuff"
+	end
+
+end
