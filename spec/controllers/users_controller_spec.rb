7045a42bf93b6d7f588666b213ffa35f165dbae8
Evan Lloyd <eulloyd@gmail.com>
2014-01-12 18:06:08 +0900

    Fills out controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe UsersController do
+	describe "GET #new" do
+		it "assigns a new User to @user"
+
+		it "renders the new view"
+	end
+
+	describe "POST #create" do
+		context "with valid attributes" do
+			it "saves a new user in the database"
+			it "redirects to root"
+		end
+
+		context "with invalid attributes" do
+			it "does not save the user in the database"
+			it "renders User#new"
+		end
 
 end
