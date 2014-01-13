42ecf8becfbf47939593ecf2de79489cd1c2bf3a
Evan Lloyd <eulloyd@gmail.com>
2014-01-13 14:22:06 +0900

    Corrects article/category relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140112082003) do
+ActiveRecord::Schema.define(version: 20140112074603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20140112082003) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "categories", force: true do |t|
